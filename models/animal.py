8137a01009c62ac7ccbc64151c4bc3bb95498dd4
edel <edel.omondi@student.moringaschool.com>
2025-07-16 11:31:42 +0300

    created route for animal

--- models/animal.py
+++ models/animal.py
@@ -12,6 +12,8 @@ class Animal(db.Model):
     type = db.Column(db.String(50)) 
     is_sold = db.Column(db.Boolean, default=False)
     
+    picture_url = db.Column(db.String(255)) 
+
     farmer_id = db.Column(db.Integer, db.ForeignKey('farmers.id'), nullable=False)
 
     def to_dict(self):
@@ -23,5 +25,6 @@ class Animal(db.Model):
             "price": self.price,
             "type": self.type,
             "is_sold": self.is_sold,
-            "farmer_id": self.farmer_id
+            "farmer_id": self.farmer_id,
+            "picture_url": self.picture_url  
         }
