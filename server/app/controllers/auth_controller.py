fb47d65ec1b73f2c61ecfcd0eff0b72306c34bb6
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-07-24 16:05:50 +0300

    Converting user_id to an integer: Line 76

--- server/app/controllers/auth_controller.py
+++ server/app/controllers/auth_controller.py
@@ -73,7 +73,7 @@ def login_user():
 @jwt_required()
 def get_profile():
     user_id = get_jwt_identity()
-    user = User.query.get(user_id)
+    user = User.query.get(int(user_id))
 
     if not user:
         return jsonify({"error": "User not found"}), 404
