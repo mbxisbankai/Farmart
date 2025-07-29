ca21fd85529c86e091d3812dfd3dc93e64c43579
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-07-29 10:46:41 +0300

    Payment controller fix

--- server/app/run.py
+++ server/app/run.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from app.extensions import db, migrate, jwt
 from app.models import user, cart, animal, order, payment, farmer
 from app.controllers.order_controller import OrderController, OrderControllerOne
-from app.routes import auth_bp, animal_bp, user_bp, cart_bp, farmer_bp, order_bp, PaymentController, PaymentControllerOne
+from app.routes import auth_bp, animal_bp, user_bp, cart_bp, farmer_bp, order_bp, payment_bp
 
 
 app = Flask(__name__)
@@ -21,14 +21,15 @@ CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 app.register_blueprint(animal_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(cart_bp)
-# app.register_blueprint(farmer_bp)
+app.register_blueprint(farmer_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(payment_bp)
 
 api.add_resource(OrderController, '/orders')
 api.add_resource(OrderControllerOne, '/order/<int:id>')
-api.add_resource(PaymentController, '/payments')
-api.add_resource(PaymentControllerOne, '/payment/<int:id>')
+# api.add_resource(PaymentController, '/payments')
+# api.add_resource(PaymentControllerOne, '/payment/<int:id>')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
