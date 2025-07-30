55261af278ac4a840eb75a553f462d75623b678d
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-07-30 09:28:12 +0300

    Fixed import error in BuyerPage.jsx

--- client/src/pages/BuyerPage.jsx
+++ client/src/pages/BuyerPage.jsx
@@ -1,6 +1,7 @@
 // src/pages/BuyerPage.jsx
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
+import api from "../api/axios";
 import {
   Container,
   Row,
